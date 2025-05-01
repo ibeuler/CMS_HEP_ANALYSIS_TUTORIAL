f8cd70b80a5d5b6c2ab420309534c1a840bbc6ea
ibeuler <ibohamed27@gmail.com>
2025-05-01 20:29:40 +0300

    first commit

---
+++ MyMuon.C
@@ -0,0 +1,14 @@
+#include "MyMuon.h"
+#include <iostream>
+
+MyMuon::MyMuon() {
+    // Constructor: no event data access here!
+    // Maybe initialize counters, histograms, etc.
+}
+
+MyMuon::~MyMuon() {
+    // Destructor: clean up if needed
+}
+
+
+
